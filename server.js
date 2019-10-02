db179f9898765edb4ce66a85d680a434401e771f
Terence Lee <leet23@gmatc.matc.edu>
2019-10-01 21:10:57 -0500

    Add data validation for user info

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import connectDatabase from './config/db';
+import {check, validationResult} from 'express-validator';
 
 //Initialize express application
 const app = express();
@@ -23,9 +24,17 @@ app.get('/', (req, res) =>
     @route POST api/users
     @desc Register user
 */
-app.post('/api/users', (req, res) =>{
-    console.log(req.body);
-    res.send(req.body);
+app.post('/api/users', [
+    check('name', "Please enter your name").not().isEmpty(),
+    check('email', "Please enter your email").isEmail(),
+    check('password', "Please enter a password with at least 6 characters").isLength({min: 6})
+], (req, res) =>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(422).json({errors: errors.array()});
+    } else {
+        res.send(req.body);
+    }
 });
 
 //Connection listener

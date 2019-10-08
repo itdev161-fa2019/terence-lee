b1e775f5b93523315243e9a15ea70a41a449faa7
Terence Lee <leet23@gmatc.matc.edu>
2019-10-08 00:06:28 -0500

    Completed Activity 4

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import connectDatabase from './config/db';
 import {check, validationResult} from 'express-validator';
+import cors from 'cors';
 
 //Initialize express application
 const app = express();
@@ -10,6 +11,9 @@ connectDatabase();
 
 //Configure middleware
 app.use(express.json({extended: false}));
+app.use(cors({
+    origin: "http://localhost:3000"
+}));
 
 //API endpoints
 /*

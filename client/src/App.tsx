834310902f1720a5d84fb83ae8dca817e9de1b1a
Terence Lee <leet23@gmatc.matc.edu>
2019-10-30 20:47:54 -0500

    Finished Activity 7

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import './App.css';
 import axios from 'axios';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import './App.css';
+import Register from './components/Register/Register';
+import Login from './components/Login/Login';
+
 
 class App extends React.Component {
   state = {
@@ -30,10 +34,10 @@ class App extends React.Component {
                 <Link to="/">Home</Link>
               </li>
               <li>
-                <Link to="/register"></Link>
+                <Link to="/register">Register</Link>
               </li>
               <li>
-                <Link to="/login"></Link>
+                <Link to="/login">Login</Link>
               </li>
             </ul>
         </header>
@@ -42,12 +46,8 @@ class App extends React.Component {
               {this.state.data}
           </Route>
           <Switch>
-            <Route path="/register">
-              Register
-            </Route>
-            <Route path="/login">
-              Login
-            </Route>
+            <Route path="/register" component={Register}/>
+            <Route path="/login" component={Login}/>
           </Switch>
         </main>
       </div>
